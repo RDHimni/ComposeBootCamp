0f8238c2fbf9db4e36058c28f7f924e1256a6304
rida <rida.dhimni@sowit.fr>
2024-04-16 14:02:53 +0100

    * Plant Item

---
+++ app/src/main/java/ridaz/sl/composebootcamp/data/resource/DataResource.kt
@@ -0,0 +1,17 @@
+package ridaz.sl.composebootcamp.data.resource
+
+import androidx.compose.ui.graphics.Color
+
+object DataResource {
+    fun colors(): ArrayList<Color> = arrayListOf(
+        Color(33, 33, 33),
+        Color(255, 171, 145),
+        Color(255, 255, 217, 255),
+        Color(250, 250, 250),
+        Color(255, 87, 34, 255),
+        Color(103, 58, 183, 255),
+        Color(81, 164, 255, 255),
+        Color(0, 77, 64),
+        )
+
+}
